75ebd0d4497e37eb61dbdfcf2505f6a73e5357bf
AleksandarBuk <aleksandar.buk@gmail.com>
2023-06-13 12:24:17 +0200

    Changed the error in code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./assets/images/interior-cleaning.jpeg')
-filter_img = img.convert('L')
+img = Image.open('./assets/images/Website.png')
+filter_img = img
 filter_img = filter_img.filter(ImageFilter.SHARPEN)
 resize = filter_img.resize((1920,1080))
-resize.save('./assets/resized_photos/interiorhd.png')
+resize.save('./assets/resized_photos/TravelBlog.png')
 resize.show()
