10eb990c7313868a4eb946fd6723737ca014c2c0
AleksandarBuk <aleksandar.buk@gmail.com>
2023-08-01 07:22:39 +0200

    Merged script into 1 file

--- main.py
+++ main.py
@@ -1,33 +1,72 @@
+import tkinter as tk
+from tkinter import filedialog
 from PIL import Image, ImageFilter
-<<<<<<< HEAD
-import sys
+import subprocess
 import os
 
-if len(sys.argv) != 5:
-    print("Usage: main.py <image_path> <new_width> <new_height> <save_path>")
-    sys.exit(1)
+def select_image():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        img_path_entry.delete(0, tk.END)
+        img_path_entry.insert(0, file_path)
 
-image_path = sys.argv[1]
-new_width = int(sys.argv[2])
-new_height = int(sys.argv[3])
-save_path = sys.argv[4]
+def on_resize_click():
+    file_path = img_path_entry.get()
+    if not file_path:
+        return
 
-image_path = os.path.abspath(image_path)
+    try:
+        new_width = int(width_entry.get())
+        new_height = int(height_entry.get())
 
-try:
-    img = Image.open(image_path)
-    filter_img = img.filter(ImageFilter.SHARPEN)
-    resized_img = filter_img.resize((new_width, new_height))
-    resized_img.save(save_path, format='png')
-    resized_img.show()
-except Exception as e:
-    print(f"Error: {e}")
-=======
+        save_path = filedialog.asksaveasfilename(defaultextension=".jpg", filetypes=[("JPEG files", "*.jpg")])
+        if save_path:
+            # Call the image processing function
+            process_image(file_path, new_width, new_height, save_path)
+    except Exception as e:
+        error_label.config(text=f"Error: {e}")
 
-img = Image.open('./assets/images/Website.png')
-filter_img = img
-filter_img = filter_img.filter(ImageFilter.SHARPEN)
-resize = filter_img.resize((1920,1080))
-resize.save('./assets/resized_photos/TravelBlog.png')
-resize.show()
->>>>>>> origin/main
+def process_image(image_path, new_width, new_height, save_path):
+    # Get the absolute file path of the image
+    image_path = os.path.abspath(image_path)
+
+    try:
+        img = Image.open(image_path)
+        filter_img = img.filter(ImageFilter.SHARPEN)
+        resized_img = filter_img.resize((new_width, new_height))
+        resized_img.save(save_path, format='JPEG')  # Save as JPG format
+        resized_img.show()
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Create the main window
+root = tk.Tk()
+root.title("Image Resizer")
+
+# Image Selection
+img_path_label = tk.Label(root, text="Select an image:")
+img_path_label.pack()
+img_path_entry = tk.Entry(root, width=50)
+img_path_entry.pack()
+select_btn = tk.Button(root, text="Browse", command=select_image)
+select_btn.pack()
+
+# Resize Options
+width_label = tk.Label(root, text="New width:")
+width_label.pack()
+width_entry = tk.Entry(root)
+width_entry.pack()
+
+height_label = tk.Label(root, text="New height:")
+height_label.pack()
+height_entry = tk.Entry(root)
+height_entry.pack()
+
+resize_btn = tk.Button(root, text="Resize and Save", command=on_resize_click)
+resize_btn.pack()
+
+# Error Label
+error_label = tk.Label(root, text="", fg="red")
+error_label.pack()
+
+root.mainloop()
